12c36c2d155ed046a8b682a889b155ad8d5dc8ce
lux <luischn94@gmail.com>
2021-01-21 17:57:24 -0300

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+
+  resources :cocktails, only: [:index, :show, :create, :new] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: :destroy
+
+end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+puts "Adding 3 ingredients ..."
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts "Done."
